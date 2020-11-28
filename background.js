976c34832312e34430c60f36265b243209c5e5a7
seongsp <sspark114@gmail.com>
2020-11-27 21:18:40 -0800

    first commit

---
+++ background.js
@@ -0,0 +1,85 @@
+
+console.log('start program');
+
+// Called when the user inputs commands
+function onCommand(command) {
+    console.log('onCommand');
+    chrome.tabs.query({ url: 'https://open.spotify.com/*' }, function(tabs) {
+
+        for (var tab of tabs) {
+            if (tab.url.startsWith('https://open.spotify.com')) {
+                
+                if (command === 'skip') {
+                    console.log('SKIP');
+                    chrome.tabs.executeScript(tab.id, {code: 'document.querySelector(".spoticon-skip-forward-16").click()'});
+
+                } else if (command === 'previous') {
+                    console.log('PREVIOUS');
+                    chrome.tabs.executeScript(tab.id, {code: 'document.querySelector(".spoticon-skip-back-16").click()'});
+
+                } else if (command === 'play-pause') {
+                    console.log('PLAY-PAUSE');
+                    chrome.tabs.executeScript(tab.id, {code: '(document.querySelector(".spoticon-pause-16") || document.querySelector(".spoticon-play-16")).click()'});
+               
+                } else { //BLOCKS and SKIPS SONG
+                    // does not block if the song is paused
+                    if(!tab.title.startsWith('Spotify')) {
+                        console.log('BLOCKS and SKIPS SONG: ' + tab.title);
+                        chrome.tabs.executeScript(tab.id, {code: 'document.querySelector(".spoticon-skip-forward-16").click()'});
+                        setBlockedSong(tab.title);
+                    }
+                    else {
+                        console.log('Song is currently paused, cannot block.');
+                    }
+                }                
+                break;
+            }  
+        }   
+    });
+};
+
+// Adds songTitle to the list of blocked songs
+function setBlockedSong(songTitle) {
+    chrome.storage.sync.get('blockList', function(result) {
+
+            var blockArray =  result.blockList;
+            if (typeof blockArray === 'undefined') {
+                chrome.storage.sync.set({'blockList': [songTitle,]}, function() {
+                    console.log('blockList is ' + [songTitle,] + " (1) ")
+                }); 
+
+            } else {
+                blockArray.push(songTitle);
+                chrome.storage.sync.set({'blockList': blockArray}, function() {
+                    console.log('blockList is ' + blockArray + " (" + blockArray.length  + ") ")
+                }); 
+            }
+    });
+};
+
+function getBlockList() {
+    chrome.storage.sync.get('blockList', function(result) {
+        var blockArray = result.blockList;
+        return blockArray;
+    });
+};
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    console.log('updated from background' + tab.title);
+    
+    chrome.storage.sync.get('blockList', function(result) {
+        var blockArray = result.blockList;
+        if (typeof blockArray !== 'undefined' && blockArray.includes(tab.title)) {
+            //skips song
+            console.log('Previously Blocked Song is Skipped: ' + tab.title);
+            chrome.tabs.executeScript(tab.id, {code: 'document.querySelector(".spoticon-skip-forward-16").click()'});
+            console.log('song skipped');
+            
+        } else {
+            console.log('Nothing');
+        }
+    });
+});
+
+// If commmand inputs are inputted by the user, onCommand is called
+chrome.commands.onCommand.addListener(onCommand);
---
+++ popup.js
@@ -0,0 +1,42 @@
+
+chrome.storage.sync.get('blockList', function(result) {
+    var blockArray = result.blockList;
+    var blockString = '<table>';
+    if(blockArray != undefined) {
+        for (let index = 0; index < blockArray.length; index++) {
+            blockString += '<tr class="tablerow">' + 
+                '<td><button class="delBut" id="' + blockArray[index] + '">X</button><td>' +
+                '<td class="songtitle">' + blockArray[index] + '</td></tr>';    
+        };
+            
+        blockString += '</table>';
+        document.getElementById("blockList").innerHTML = blockString;
+    }
+});
+
+function clearBlockedList() {
+    chrome.storage.sync.clear( function(){
+        console.log("CLEARED");
+    });
+};
+
+function deleteSong(songTitle) {
+    chrome.storage.sync.get('blockList', function(result) {
+        var blockArray = result.blockList;
+        const index = blockArray.indexOf(songTitle);
+        if(index >= 0) {
+            blockArray.splice(index, 1);
+        }
+        chrome.storage.sync.set({'blockList': blockArray}, function() {
+            console.log('removed ' + songTitle);
+        });
+    });
+};
+
+$(document).ready(function() {
+    $("button").click(function(){
+        $(this).closest("tr").remove();
+        deleteSong(this.id);
+        //clearBlockedList();
+    });
+});
